e7fb202097748061000f2a2fd69e5d4f78be9ef2
Adán <21890241@ittizmin.edu.mx>
2024-06-10 23:01:29 -0600

    adding picocolors and node modules

--- ls-advance.js
+++ ls-advance.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const picocolors = require('picocolors')
 
 const folder = process.argv[2] ?? '.' // poder pasar una carpeta como argumento
 
@@ -8,7 +9,7 @@ async function ls (folder) {
     try {
         files = await fs.readdir(folder) // asincronia secuencial
     } catch{
-        console.error(`No se pudo leer el directorio ${folder}`)
+        console.error(picocolors.red(`No se pudo leer el directorio ${folder}`))
         process.exit(1)
     }
 
@@ -16,7 +17,8 @@ async function ls (folder) {
         const filePath = path.join(folder, file)
         let stats
         try {
-            stats = await fs.stat(filePath)
+            stats = await fs.stat(filePath) // map es un callback y no espera el await
+            // mapea todas de una vez
         } catch {
             console.error(`No se pudo leer el directorio ${folder}`)
             process.exit(1)
@@ -24,10 +26,10 @@ async function ls (folder) {
 
         const isDirectory = stats.isDirectory()
         const fileType = isDirectory ? '/' : '-'
-        const fileSize = stats.size
+        const fileSize = stats.size.toString()
         const fileModified = stats.mtime.toLocaleDateString()
 
-        return `${fileType} ${file.padEnd(30)} ${fileSize.toString().padStart(10)} ${fileModified}`
+        return `${fileType} ${picocolors.blue(file.padEnd(30))} ${picocolors.green(fileSize.padStart(10))} ${picocolors.yellow(fileModified)}`
     })
 
 
