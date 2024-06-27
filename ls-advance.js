59686b21061f465826cad5979b06bef7a202bc98
Adán <21890241@ittizmin.edu.mx>
2024-06-27 13:35:30 -0600

    Adding format module

--- ls-advance.js
+++ ls-advance.js
@@ -5,39 +5,38 @@ const picocolors = require('picocolors')
 const folder = process.argv[2] ?? '.' // poder pasar una carpeta como argumento
 
 async function ls (folder) {
-    let files
+  let files
+  try {
+    files = await fs.readdir(folder) // asincronia secuencial
+  } catch {
+    console.error(picocolors.red(`No se pudo leer el directorio ${folder}`))
+    process.exit(1)
+  }
+
+  const filesPromises = files.map(async file => {
+    const filePath = path.join(folder, file)
+    let stats
     try {
-        files = await fs.readdir(folder) // asincronia secuencial
-    } catch{
-        console.error(picocolors.red(`No se pudo leer el directorio ${folder}`))
-        process.exit(1)
+      stats = await fs.stat(filePath) // map es un callback y no espera el await
+      // mapea todas de una vez
+    } catch {
+      console.error(`No se pudo leer el directorio ${folder}`)
+      process.exit(1)
     }
 
-    const filesPromises = files.map(async file => {
-        const filePath = path.join(folder, file)
-        let stats
-        try {
-            stats = await fs.stat(filePath) // map es un callback y no espera el await
-            // mapea todas de una vez
-        } catch {
-            console.error(`No se pudo leer el directorio ${folder}`)
-            process.exit(1)
-        }
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? '/' : '-'
+    const fileSize = stats.size.toString()
+    const fileModified = stats.mtime.toLocaleDateString()
 
-        const isDirectory = stats.isDirectory()
-        const fileType = isDirectory ? '/' : '-'
-        const fileSize = stats.size.toString()
-        const fileModified = stats.mtime.toLocaleDateString()
+    return `${fileType} ${picocolors.blue(file.padEnd(30))} ${picocolors.green(fileSize.padStart(10))} ${picocolors.yellow(fileModified)}`
+  })
 
-        return `${fileType} ${picocolors.blue(file.padEnd(30))} ${picocolors.green(fileSize.padStart(10))} ${picocolors.yellow(fileModified)}`
-    })
+  const filesInfo = await Promise.all(filesPromises)
 
-
-    const filesInfo = await Promise.all(filesPromises)
-
-    filesInfo.forEach(fileInfo => {
-        console.log(fileInfo)
-    });
+  filesInfo.forEach(fileInfo => {
+    console.log(fileInfo)
+  })
 }
 
 ls(folder)
--- ls.js
+++ ls.js
@@ -13,14 +13,13 @@ const fs = require('node:fs/promises')
 }) */
 
 fs.readdir('.')
-    .then(files => { 
-        files.forEach(file => {
-            console.log(file)
-        });
-})
-.catch(err => {
-    if (err){
-        console.error('Error al leer el directorio:', err)
-        return
+  .then(files => {
+    files.forEach(file => {
+      console.log(file)
+    })
+  })
+  .catch(err => {
+    if (err) {
+      console.error('Error al leer el directorio:', err)
     }
-})
+  })
