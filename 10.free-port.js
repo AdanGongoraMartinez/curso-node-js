273c7ce8d22f6c69cff0394ce018a5ebaf7d3db7
adan <adangonmar@gmail.com>
2024-09-07 21:34:59 -0600

    free port module

---
+++ 10.free-port.js
@@ -0,0 +1,27 @@
+const net = require('node:net') // protocolo TCP
+
+function findAvailablePort (desiredPort) {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer()
+
+    server.listen(desiredPort, () => {
+      // comproabr si el puerto deseado esta disponible y retornarlo
+      const port = server.address().port
+
+      server.close(() => {
+        resolve(port)
+      })
+    })
+
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        // si el puerto deseado NO esta disponible, encontrar un puerto disponible y retornarlo
+        findAvailablePort(0).then(port => resolve(port))
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+module.exports = { findAvailablePort }
--- 9.http.js
+++ 9.http.js
@@ -1,10 +1,18 @@
-const http = require('node:http')
+const http = require('node:http') // protocolo HTTP
+const { findAvailablePort } = require('./10.free-port.js')
+
+// variable de entorno
+// PORT=1234 node 9.http.js
+// archivo .env es parte de una biblioteca de dependencias
+const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola mundo')
 })
 
-server.listen(0, () => {
-  console.log(`server listening on port http://localhost:${server.address().port}`)
+findAvailablePort(desiredPort).then(port => {
+  server.listen(port, () => {
+    console.log(`server listening on port http://localhost:${port}`)
+  })
 })
